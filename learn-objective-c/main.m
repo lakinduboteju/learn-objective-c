bfbf62c495e695c1700254bd3d54d381d8683024
Lakindu Boteju <lakinduboteju@gmail.com>
2022-05-01 10:36:27 +0000

    Created a Command-line project with Xcode 13

---
+++ learn-objective-c/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  learn-objective-c
+//
+//  Created by ec2-user on 5/1/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
